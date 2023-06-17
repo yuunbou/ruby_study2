608a7f76cfb79329a5d81fff8589acb01cdbc53e
yuunbou <112618391+yuunbou@users.noreply.github.com>
2023-06-17 14:15:49 +0000

    [Study]配列の多重代入

--- sample_ruby.rb
+++ sample_ruby.rb
@@ -1,22 +1,12 @@
-#  要素の変更、追加、削除
+# divmodは商と余を配列で返す
 
-a = [1, 2, 3]
-puts a.delete_at(1)
-puts a
+puts 14.divmod(3)
 
+# 戻り値を配列のまま受け取る
+quo_rem = 14.divmod(3)
+puts "商=#{quo_rem[0]},余り=#{quo_rem[1]}"
 
-# 配列を使って多重代入
-a, b = 1, 2
-puts a
-puts b
-
-# 右辺の数が少ない場合はnilが入る
-c, d = [10]
-puts c
-puts d
-
-# 右辺の数が多い場合ははみ出した値が切り捨てられる
-e,f = [100, 200, 300]
-puts e
-puts f
+# 多重代入で別々の変数として受け取る
+quotient, remainder = 14.divmod(3)
+puts "商=#{quotient}, 余り=#{remainder}"
 
