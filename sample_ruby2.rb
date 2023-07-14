684053ec72505cd1ae1ff440d4c309e01fd81bef
yuunbou <112618391+yuunbou@users.noreply.github.com>
2023-07-14 16:35:04 +0000

    [Study]範囲

--- sample_ruby2.rb
+++ sample_ruby2.rb
@@ -1,24 +1,21 @@
-# joinとsum
+# 範囲（Range）
 
-chars = ['a', 'b', 'c']
-puts chars.join
-# 区切り文字をハイフンにして各要素を連結する
-puts chars.join('-')
+puts (1..5).class
+puts (1..5).class
 
-# 先頭に'>'をつけ、各要素を大文字にして連結する
-puts chars.sum('>') { |c| c.upcase }
+#..を使うと５が範囲に含まれる（１以上５以下）
+range = 1..5
+puts range.include?(0)
+puts range.include?(1)
+puts range.include?(4.9)
+puts range.include?(5)
+puts range.include?(6)
 
-data = ['a', 2, 'b', 4]
-# 配列に数値が含まれていても連結可能
-puts data.join
-
-# ＆とシンボルを使ってもっと簡潔にかく
-
-# このコード（Before）
-puts ['ruby', 'java', 'python'].map { |s| s.upcase }
-# 簡潔に書き換え（after）
-puts ['ruby', 'java', 'python'].map(&:upcase)
-
-puts [1, 2, 3, 4, 5, 6].select { |n| n.odd? }
-puts [1, 2, 3, 4, 5, 6].select(&:odd?)
+#...を使うと５が範囲に含まれない（1以上5未満）
+range = 1...5
+puts range.include?(0)
+puts range.include?(1)
+puts range.include?(4.9)
+puts range.include?(5)
+puts range.include?(6)
 
